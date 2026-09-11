e56e753d71f1b29e01e1ce07104a9028cb15b7a0
Mahima <rjoannmahima@gmail.com>
2026-09-12 01:44:13 +0800

    Version 1

--- auditor.py
+++ auditor.py
@@ -1,5 +1,20 @@
 inventory = 0
+error = 0
 while True:
-    inventory2 = input("Enter a stock quantity")
-    if inventory2.lower() == "quit":
+    stock = input("Enter a stock quantity: ")
+    if stock.lower() == "quit" or (stock.isdigit() and int(stock) > 500):
+        print("Total Units Processed: ", inventory)
+        print("Number of Failed Entries: ", error)
         break
+    elif stock.isdigit() and int(stock) > 0:
+        stock = int(stock)
+        inventory += stock
+        continue
+    else:
+        error += 1
+        print("Error: Please enter a valid number")
+        continue
+
+
+
+        
