07e820a58caf44c857f710e7162f553bdd35a498
Mahima <rjoannmahima@gmail.com>
2026-09-18 14:56:17 +0800

    First functions

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,3 +1,18 @@
+def get_valid_input():
+    stock = input("Enter a stock quantity: ")
+    return stock
+def process_delivery(current_total, new_value):
+    return current_total + new_value
+
+def calculate_tax(amount):
+    tax_rate = 0.10
+    return tax_rate * amount
+
+def generate_report(total_units, failed_attempts):
+    print("Total Units Processed: ", total_units)
+    print("Number of Failed Entries: ", failed_attempts)
+
+
 inventory = 0
 error = 0
 
@@ -19,6 +34,7 @@ while True:
             break
 
         inventory += int(stock)
+        tax_amount = tax(inventory)
         
     else:
         error += 1
